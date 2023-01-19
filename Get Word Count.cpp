7758a751fb1dda83a3a55cf93a67310b755dfae6
Truancy-A4S1 <104305411+Truancy-A4S1@users.noreply.github.com>
2023-01-19 21:25:56 +0800

    Update Get Word Count.cpp

--- Get Word Count.cpp
+++ Get Word Count.cpp
@@ -1,30 +1,35 @@
-// Create a function that takes a string and returns the word count. The string will be a sentence.
+// Create a function that takes a string and returns the word count.
 
 #include<iostream>
 #include<string>
+
 using namespace std;
 
 int wordCount(string input)
 {
     int count=1, i=0;
 
-    while(input[i]!= '\0')
+    if(input[i]==' ' && input[i+1]== '\0'){ // if the string is empty
+        return 0;
+    }
+
+    while(input[i]!= '\0')  // loop until the end of the string
     {
-        if(input[i] == ' ')
+        if(input[i] == ' ' && input[i+1] != '\0') // add word count if there is a space and a character after the space
         {
             count++;
         }
     i++;
     }
-    return count;
+    
+    return count; // return count after looping
 }
 
 int main()
 {
-    cout << "Enter a text: " << endl;
-    string in;
-    getline(cin, in);
-
-    cout << "\n\nWord Count: ";
-    cout << wordCount(in);
+    cout << "Enter a text: ";
+    string text;
+    getline(cin, text);
+    cout << "\nWord Count: ";
+    cout << wordCount(text) << endl;
 }
